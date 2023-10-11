f92004932ea92fe613a4b6e7f22479c6b34e191b
AdiSatR7 <19104027@ittelkom-pwt.ac.id>
2023-10-12 03:07:58 +0700

    Started working

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.tsx",
+    "./**/*.{tsx,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ types/index.ts
@@ -0,0 +1,34 @@
+export type User = {
+  id: number
+  email: string
+  firstName: string
+  lastName: string
+  imageUrl: string
+  level: number
+  exp: number
+}
+
+export type UsersQuery = {
+  page: number
+  perPage: number
+  total: number
+  totalPages: number
+  data: User[]
+}
+
+export type InvolvedUser = User & {
+  isCompleted: boolean
+  completedAt: string
+}
+
+export type Task = {
+  id: number
+  title: string
+  madeBy: User
+  description: string
+  isCompleted: boolean
+  createdAt: string
+  updatedAt?: string
+  completedAt: string
+  involved: InvolvedUser[]
+}
