f88f2822ef8d79b3c70a26b3011d9a025e3550f3
AdiSatR7 <19104027@ittelkom-pwt.ac.id>
2023-10-12 12:58:33 +0700

    Refactors

--- app/onboarding/login.tsx
+++ app/onboarding/login.tsx
@@ -1,21 +1,14 @@
 import { router } from "expo-router"
 import { useState } from "react"
-import {
-  ImageBackground,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  View
-} from "react-native"
-import image from "../../assets/backgrounds/login.jpg"
+import { Text, TouchableOpacity } from "react-native"
 import GlassCard from "../../components/containers/GlassCard"
 import EmailIcon from "../../components/icons/EmailIcon"
 import LockIcon from "../../components/icons/LockIcon"
 import Entry from "../../components/inputs/Entry"
 import LongButton from "../../components/inputs/LongButton"
-import { styles } from "../../styles"
+import OnboardingLayout from "../../components/layouts/OnboardingLayout"
 
-export default function LoginPage() {
+export default function LoginScreen() {
   const [emailInput, setEmailInput] = useState<string>("")
   const [rawPasswordInput, setRawPasswordInput] = useState<string>("")
 
@@ -36,43 +29,41 @@ export default function LoginPage() {
   }
 
   return (
-    <ImageBackground source={image} blurRadius={4} className="h-full -z-10">
-      <View className={`absolute z-0 flex w-full h-full blur-lg ${styles.loginOverlay}`}/>
-      <SafeAreaView className="z-10 flex flex-col h-screen m-[18px] items-center justify-center">
-        <GlassCard className="py-[12px]">
+    <OnboardingLayout>
 
-          {/* Header */}
-          <Text className="text-center text-white text-heading-1 mt-[4px] mb-[12px]">
-            Login
+      <GlassCard className="py-[12px]">
+
+        {/* Header */}
+        <Text className="text-center text-white text-heading-1 mt-[4px] mb-[12px]">
+          Login
+        </Text>
+
+        {/* Form */}
+        <Entry
+          placeholder="Masukkan email"
+          icon={EmailIcon}
+          value={emailInput}
+          setValue={setEmailInput}
+        />
+        <Entry
+          placeholder="Masukkan kata sandi"
+          icon={LockIcon}
+          value={rawPasswordInput}
+          setValue={setRawPasswordInput}
+          type="password"
+        />
+
+        {/* Register Button */}
+        <TouchableOpacity activeOpacity={0.5} onPress={handleRegister}>
+          <Text className=" text-white text-caption -mt-[4px] mb-[6px] mx-[2px]">
+            Belum punya akun? Klik disini
           </Text>
+        </TouchableOpacity>
 
-          {/* Form */}
-          <Entry
-            placeholder="Masukkan email"
-            icon={EmailIcon}
-            value={emailInput}
-            setValue={setEmailInput}
-          />
-          <Entry
-            placeholder="Masukkan kata sandi"
-            icon={LockIcon}
-            value={rawPasswordInput}
-            setValue={setRawPasswordInput}
-            type="password"
-          />
+        {/* Login Button */}
+        <LongButton label="Masuk" onClick={handleLogin} />
 
-          {/* Register Button */}
-          <TouchableOpacity activeOpacity={0.5} onPress={handleRegister}>
-            <Text className=" text-white text-caption -mt-[4px] mb-[6px] mx-[2px]">
-              Belum punya akun? Klik disini
-            </Text>
-          </TouchableOpacity>
-
-          {/* Login Button */}
-          <LongButton label="Masuk" onClick={handleLogin} />
-
-        </GlassCard>
-      </SafeAreaView>
-    </ImageBackground>
+      </GlassCard>
+    </OnboardingLayout>
   )
 }
--- app/onboarding/register.tsx
+++ app/onboarding/register.tsx
@@ -1,12 +1,5 @@
-import { Link, router } from "expo-router"
 import { useState } from "react"
-import {
-  ImageBackground,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  View
-} from "react-native"
+import { Text, View } from "react-native";
 import GlassCard from "../../components/containers/GlassCard"
 import BriefcaseIcon from "../../components/icons/BriefcaseIcon"
 import EmailIcon from "../../components/icons/EmailIcon"
@@ -15,10 +8,11 @@ import NameIcon from "../../components/icons/NameIcon"
 import BackButton from "../../components/inputs/BackButton"
 import Entry from "../../components/inputs/Entry"
 import LongButton from "../../components/inputs/LongButton"
+import OnboardingLayout from "../../components/layouts/OnboardingLayout"
 import { styles } from "../../styles"
-import image from "../../assets/backgrounds/login.jpg"
 
-export default function LoginPage() {
+
+export default function RegisterScreen() {
   const [emailInput, setEmailInput] = useState<string>("")
   const [rawPasswordInput, setRawPasswordInput] = useState<string>("")
 
@@ -31,75 +25,73 @@ export default function LoginPage() {
   }
 
   return (
-    <ImageBackground source={image} blurRadius={4} className="h-full -z-10">
-      <View className={`absolute z-0 flex w-full h-full blur-lg ${styles.loginOverlay}`}/>
-      <SafeAreaView className="z-10 flex flex-col h-screen m-[18px] items-center justify-center">
-        <GlassCard className="py-[12px]">
+    <OnboardingLayout>
 
-          {/* Header */}
-          <View className="flex-row items-center w-full bg-red-0 h-fit">
-            <BackButton />
-            <Text className="text-center text-white text-heading-1 mt-[4px] w-full">
-              Daftar Akun Baru
-            </Text>
-          </View>
+      <GlassCard className="py-[12px]">
 
-          {/* Upload picture | Non functional */}
-          <View className="flex-col items-center justify-center w-full h-fit pb-[8px]">
-            <View className={`rounded-full w-[84px] h-[84px] bg-gray-500 ${styles.glassOutline}`}/>
-            <Text className="text-bright-gray mt-[4px]">Unggah gambar</Text>
-          </View>
+        {/* Header */}
+        <View className="flex-row items-center w-full bg-red-0 h-fit">
+          <BackButton />
+          <Text className="text-center text-white text-heading-1 mt-[4px] w-full">
+            Daftar Akun Baru
+          </Text>
+        </View>
 
-          {/* Form */}
-          <View className="flex-row justify-between w-[49%]">
-            <Entry
-              placeholder="Nama depan"
-              icon={NameIcon}
-              value={emailInput}
-              setValue={setEmailInput}
-            />
-            <View className="w-[6px]" />
-            <Entry
-              placeholder="Nama belakang"
-              value={emailInput}
-              setValue={setEmailInput}
-            />
-          </View>
+        {/* Upload picture | Non functional */}
+        <View className="flex-col items-center justify-center w-full h-fit pb-[8px]">
+          <View className={`rounded-full w-[84px] h-[84px] bg-gray-500 ${styles.glassOutline}`}/>
+          <Text className="text-bright-gray mt-[4px]">Unggah gambar</Text>
+        </View>
+
+        {/* Form */}
+        <View className="flex-row justify-between w-[49%]">
           <Entry
-            placeholder="Masukkan email"
-            icon={EmailIcon}
+            placeholder="Nama depan"
+            icon={NameIcon}
             value={emailInput}
             setValue={setEmailInput}
           />
+          <View className="w-[6px]" />
           <Entry
-            placeholder="Masukkan jabatan"
-            icon={BriefcaseIcon}
+            placeholder="Nama belakang"
             value={emailInput}
             setValue={setEmailInput}
           />
-          <Entry
-            placeholder="Masukkan kata sandi"
-            icon={LockIcon}
-            value={rawPasswordInput}
-            setValue={setRawPasswordInput}
-            type="password"
-          />
-          <Entry
-            placeholder="Ulangi kata sandi"
-            icon={LockIcon}
-            value={rawPasswordInput}
-            setValue={setRawPasswordInput}
-            type="password"
-          />
+        </View>
+        <Entry
+          placeholder="Masukkan email"
+          icon={EmailIcon}
+          value={emailInput}
+          setValue={setEmailInput}
+        />
+        <Entry
+          placeholder="Masukkan jabatan"
+          icon={BriefcaseIcon}
+          value={emailInput}
+          setValue={setEmailInput}
+        />
+        <Entry
+          placeholder="Masukkan kata sandi"
+          icon={LockIcon}
+          value={rawPasswordInput}
+          setValue={setRawPasswordInput}
+          type="password"
+        />
+        <Entry
+          placeholder="Ulangi kata sandi"
+          icon={LockIcon}
+          value={rawPasswordInput}
+          setValue={setRawPasswordInput}
+          type="password"
+        />
 
-          {/* Gap */}
-          <View className="h-[1px]" />
+        {/* Gap */}
+        <View className="h-[1px]" />
 
-          {/* Register button */}
-          <LongButton label="Daftar Sekarang" onClick={handleSubmit} />
+        {/* Register button */}
+        <LongButton label="Daftar Sekarang" onClick={handleSubmit} />
 
-        </GlassCard>
-      </SafeAreaView>
-    </ImageBackground>
+      </GlassCard>
+    </OnboardingLayout>
   )
 }
