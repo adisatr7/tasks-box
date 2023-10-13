0ffce599f717c794d54546778c6e779b471b546a
AdiSatR7 <19104027@ittelkom-pwt.ac.id>
2023-10-13 11:45:17 +0700

    Added multiline option for Entry

--- components/inputs/Entry.tsx
+++ components/inputs/Entry.tsx
@@ -1,4 +1,4 @@
-import { TextInput, TextInputProps, TouchableOpacity, View } from "react-native"
+import { Text, TextInput, TextInputProps, TouchableOpacity, View } from "react-native"
 import { twMerge } from "tailwind-merge"
 import { styles } from "../../styles"
 import { SvgIcon } from "../../types"
@@ -14,8 +14,8 @@ type Props = {
   value?: string
   type?: TextInputProps["keyboardType"] | "password"
   setValue?(value: string): void
-  className?: string
   icon?: React.FC<SvgIcon>
+  multiline?: boolean
 }
 
 export default function Entry(props: Props) {
@@ -23,34 +23,39 @@ export default function Entry(props: Props) {
   const [isHidden, setIsHidden] = useState<boolean>(true)
 
   return (
-    <BlurView
-      tint="dark"
-      intensity={10}
-      className={twMerge(
-        `w-full my-[4px] flex-row px-[8px] py-[4px] text-left text-white rounded-md items-center ${styles.glassInput}`, props.className)
-      }>
-      {props.icon && <props.icon height={24} width={24} />}
-      <TextInput
-        placeholder={props.placeholder}
-        placeholderTextColor="#989898"
-        value={props.value}
-        onChangeText={props.setValue}
-        keyboardType={props.type === "password" ? "default" : props.type}
-        secureTextEntry={props.type === "password" && isHidden}
-        className="flex-1 ml-[6px] text-white"
-      />
-      {
-        props.type === "password" && (
+    <>
+      {props.label && (
+        <Text className="text-caption text-bright-gray">{props.label}</Text>
+      )}
+      <BlurView
+        tint="dark"
+        intensity={10}
+        className={`w-full my-[4px] flex-row px-[8px] py-[4px] text-left text-white rounded-md items-center ${styles.glassInput}`}>
+        {props.icon && <props.icon height={24} width={24} />}
+        <TextInput
+          placeholder={props.placeholder}
+          placeholderTextColor="#989898"
+          value={props.value}
+          onChangeText={props.setValue}
+          multiline={props.multiline ? true : false}
+          numberOfLines={props.multiline ? 4 : 1}
+          keyboardType={props.type === "password" ? "default" : props.type}
+          secureTextEntry={props.type === "password" && isHidden}
+          className={`flex-1 ml-[6px] text-white
+          ${props.multiline ? "h-[96px]" : "h-[24px]"}
+        `} />
+        {props.type === "password" && (
           <TouchableOpacity
             activeOpacity={0.5}
             onPress={() => setIsHidden(!isHidden)}>
-            {isHidden
-              ? <OpenedEyeIcon fill="#EFEFEF"/>
-              : <ClosedEyeIcon fill="#EFEFEF"/>
-            }
+            {isHidden ? (
+              <OpenedEyeIcon fill="#EFEFEF" />
+            ) : (
+              <ClosedEyeIcon fill="#EFEFEF" />
+            )}
           </TouchableOpacity>
-        )
-      }
-    </BlurView>
+        )}
+      </BlurView>
+    </>
   )
 }
