5595f948ba0a83a403144108b4e00087b3a3ac75
AdiSatR7 <19104027@ittelkom-pwt.ac.id>
2023-10-16 18:12:40 +0700

    Buttons now dismiss keyboard

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity } from "react-native"
+import { Keyboard, StyleProp, Text, TouchableOpacity, ViewStyle } from "react-native"
 import { SvgIcon } from "../../types"
 
 
@@ -6,17 +6,21 @@ type Props = {
   label: string
   icon?: React.FC<SvgIcon>
   onClick?: () => void
-  className?: string
+  style?: StyleProp<ViewStyle>
 }
 
-export default function LongButton({ label, icon: Icon, onClick, className }: Props) {
+export default function PrimaryButton({ label, icon: Icon, onClick, style }: Props) {
   return (
     <TouchableOpacity
       activeOpacity={0.5}
-      onPress={onClick}
-      className={`flex-row items-center justify-center bg-primary py-[6px] my-[4px] rounded-[6px] shadow-sm ${className}`}>
+      onPress={() => {
+        Keyboard.dismiss()
+        onClick && onClick()
+      }}
+      style={style}
+      className={`flex-row items-center justify-center bg-primary py-[6px] my-[4px] rounded-[6px] shadow-sm px-[14px] h-fit`}>
       {Icon && <Icon fill="white" />}
-      <Text className="flex-row font-bold mx-[8px] text-center text-white w-fit text-caption">
+      <Text className="flex-row font-bold ml-[8px] text-center text-white w-fit text-caption">
         {label}
       </Text>
     </TouchableOpacity>
